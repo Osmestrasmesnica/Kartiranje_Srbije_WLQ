dbedd56f31c5d27291505a59e73460b20a4dc35f
Osmestrasmesnica <117804746+Osmestrasmesnica@users.noreply.github.com>
2023-01-20 16:12:45 +0100

    Update sa faksa
    Dodao si da ti napravi za svaki UTM array

---
+++ WLQMapaV3.1/varijable.js
@@ -0,0 +1,13 @@
+//podesavanje varijabli
+const   inputs = document.querySelectorAll('.varijable input')
+function liveUpdate () {
+    const sufiks = this.dataset.sizing || '';
+    console.log(sufiks);
+    console.log(this.name);
+    document.documentElement.style.setProperty(`--${this.name}`,this.value + sufiks);
+}
+inputs.forEach(input => input.addEventListener('change', liveUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', liveUpdate));
+
+
+
